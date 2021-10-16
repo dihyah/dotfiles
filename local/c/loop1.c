ec2e84c75e9e7e2fc91257b90ddcc61905e1299d
dihyah <hiidihyah@gmail.com>
2021-10-16 17:46:09 +0800

    recap c loops

---
+++ local/c/loop1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(void){
+  int n;
+
+  printf("Enter a number for n: ");
+  scanf("%i", &n);
+
+  //for (int i = 3; i <= n; i+=3){
+  //  printf("%i\n", i);
+  //}
+
+  int i = 3;
+  //while (i <= n){
+  //  if (i % 3 == 0)
+  //  printf("%i\n", i);
+  //  i++;
+  //}
+
+  //do{
+  //  if (!(n % 2 == 0))
+  //  printf("%i\n", n);
+  //  n++;
+  //} while (n <= 50);
+}
+
