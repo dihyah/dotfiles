354f3f4c81e6ede60100424050d547089edbf618
dihyah <hiidihyah@gmail.com>
2021-11-04 14:49:07 +0800

    -tr command

--- test/bash/script.sh
+++ test/bash/script.sh
@@ -172,8 +172,8 @@ declare -p array
 
 #Example to split a string using trim (tr) command
 my_str="We;welcome;you;on;javatpoint."
-my_arr=($(echo $my_str | tr ; \n))
+my_arr=($(echo -n $my_str | tr ";" "\n"))
 
-for i in "${my_arr[@]}"; do
-  echo $i
+for s in "${my_arr[@]}"; do
+  echo $s
 done
